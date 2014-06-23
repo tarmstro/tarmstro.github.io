39bd856d8bd2a8f5ea51b934481c2f94641b7f6f
Tom Armstrong <tarmstro@gmail.com>
2014-06-23 12:36:52 -0400

    initial commit

---
+++ js/tarmstro.js
@@ -0,0 +1,7 @@
+angular.module("tarmstro_app", [])
+  .controller("MainController", function($scope){
+      $scope.data = {};
+      $scope.data.year = (new Date()).getFullYear();
+      $scope.data.name = "Tom Armstrong";
+      $scope.data.title = (new Date("2014-07-01T00:00:00.000Z")) < new Date() ? "Associate" : "Assistant";
+  });
