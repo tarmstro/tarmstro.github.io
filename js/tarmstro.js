b745f79ac6c683add7bd1075fc33d55dfde55da4
Tom Armstrong <tarmstro@gmail.com>
2014-06-24 15:36:26 -0400

    added publications and json

--- js/tarmstro.js
+++ js/tarmstro.js
@@ -1,7 +1,9 @@
+
+
 angular.module("tarmstro_app", [])
   .controller("MainController", function($scope){
-      $scope.data = {};
-      $scope.data.year = (new Date()).getFullYear();
-      $scope.data.name = "Tom Armstrong";
-      $scope.data.title = (new Date("2014-07-01T00:00:00.000Z")) < new Date() ? "Associate" : "Assistant";
+    $scope.data = {};
+    $scope.data.year = (new Date()).getFullYear();
+    $scope.data.name = "Tom Armstrong";
+    $scope.data.title = (new Date("2014-07-01T00:00:00.000Z")) < new Date() ? "Associate" : "Assistant";
   });
