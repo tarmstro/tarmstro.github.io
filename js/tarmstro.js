9eda557f4ef0542ff0a9d1695e3329e1399925a6
Tom Armstrong <tarmstro@gmail.com>
2014-06-26 15:11:00 -0400

    added publication table initial

--- js/tarmstro.js
+++ js/tarmstro.js
@@ -6,4 +6,18 @@ angular.module("tarmstro_app", [])
     $scope.data.year = (new Date()).getFullYear();
     $scope.data.name = "Tom Armstrong";
     $scope.data.title = (new Date("2014-07-01T00:00:00.000Z")) < new Date() ? "Associate" : "Assistant";
+  })
+  .controller("PublicationController", function($scope){
+  	angular.forEach(publications, function(value, key) {
+    	var pub = "<tr>";
+    	if (value["doi"] != '')
+	    	pub += "<th><a href='http://dx.doi.org/" + value["doi"] + "'>" + value["title"] + "</a></th>";
+	    else
+	    	pub += "<th>" + value["title"] + "</th>";
+    	pub += "<th>" + value["friendly_author"] + "</th>";
+    	pub += "<th>" + value["booktitle"] + "</th>";
+    	pub += "<th>" + value["year"] + "</th>";
+    	pub += "</tr>";
+    	$('#pubs').append(pub);
+		});
   });
